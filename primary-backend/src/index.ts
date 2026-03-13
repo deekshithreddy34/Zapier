ee5cb2b0f3d61b73813536b37ea39bbe41988c0f
Deekshith Reddy <sripatideekshith@gmail.com>
2026-03-13 10:31:47 +0530

    backend

---
+++ primary-backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { userRouter } from "./router/user";
+import { zapRouter } from "./router/zap";
+import cors from "cors";
+import { triggerRouter } from "./router/trigger";
+import { actionRouter } from "./router/action";
+
+const app = express();
+app.use(express.json());
+app.use(cors({origin: "http://localhost:3001",
+  credentials: true}))
+
+app.use("/api/v1/user", userRouter);
+
+app.use("/api/v1/zap", zapRouter);
+
+app.use("/api/v1/trigger", triggerRouter);
+
+app.use("/api/v1/action", actionRouter);
+
+app.listen(3000,()=>{
+    console.log("running on port 3000")
+});
