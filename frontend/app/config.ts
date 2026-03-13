2b5563477849a8353cb24287615bd17b1f8f3723
Deekshith Reddy <sripatideekshith@gmail.com>
2026-03-13 10:21:55 +0530

    frontend

---
+++ frontend/app/config.ts
@@ -0,0 +1,4 @@
+
+// export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000"
+export const BACKEND_URL="http://localhost:3000"
+export const HOOKS_URL = process.env.NEXT_PUBLIC_HOOKS_URL || "http://localhost:3002"
